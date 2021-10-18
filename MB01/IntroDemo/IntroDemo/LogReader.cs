f4285a2dbe9c88aaecee9d5ac42b55f5329978ce
symas <tk@symas.ch>
2021-10-18 13:38:37 +0200

    init

---
+++ MB01/IntroDemo/IntroDemo/LogReader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DatenstrukturenIntro {
+    // The LogReader produces synthetic data for use in experiments.
+    // It simulates a log file with 100'000 log lines and 90'000 different IP adresses.
+    public class LogReader : IEnumerable<LogLine> {
+        private int counter;
+
+        public IEnumerator<LogLine> GetEnumerator() {
+            while (counter < 100000) {
+                yield return new LogLine(counter % 90000);
+                counter++;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return this.GetEnumerator();
+        }
+    }
+
+    public class LogLine {
+        readonly int counter;
+        public LogLine(int counter) {
+            this.counter = counter;
+        }
+
+        // The return value is not an IP-address, but just a string.
+        // The strings will be unique to the extend of the 'counter' parameter.
+        public string GetIP() {
+            return "ip" + counter;
+        }
+    }
+}
---
+++ MB01/IntroDemo/IntroDemo/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DatenstrukturenIntro {
+    public class Program {
+        static void Main(string[] args) {
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            Console.WriteLine("\nStart ReadAllLogs");
+            var lineCount = ReadAllLogs();
+            Console.WriteLine("Number of lines: " + lineCount);
+            Console.WriteLine("Time elapsed: {0:0.0000}", stopWatch.ElapsedMilliseconds / 1000.0);
+
+
+            // *** 1x mit List<> und 1x mit HashSet<> in CountUniqueIPs
+            stopWatch.Restart();
+            Console.WriteLine("\nStart CountUniqueIPs");
+            var ipCount = CountUniqueIPs();
+            Console.WriteLine("Number of unique IPs: " + ipCount);
+            Console.WriteLine("Time elapsed: {0:0.0000}", stopWatch.ElapsedMilliseconds / 1000.0);
+
+
+            Console.ReadLine();
+        }
+
+
+        private static int ReadAllLogs() {
+            var logReader = new LogReader();
+            var linesSeen = 0;
+            foreach (var line in logReader) {
+                var ip = line.GetIP();
+                linesSeen++;
+            }
+            return linesSeen;
+        }
+
+        private static int CountUniqueIPs() {
+            var logReader = new LogReader();
+            var ipsSeen = new List<string>();
+            //var ipsSeen = new HashSet<string>();
+            foreach (var logLine in logReader) {
+                var ip = logLine.GetIP();
+                if (!ipsSeen.Contains(ip))
+                    ipsSeen.Add(ip);
+            }
+            return ipsSeen.Count;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //private static int CountUniqueIPs()
+        //{
+        //    var logReader = new LogReader();
+        //    var ipsSeen = new List<string>();
+        //    //var ipsSeen = new HashSet<string>();
+        //    foreach (var logLine in logReader)
+        //    {
+        //        var ip = logLine.GetIP();
+        //        if (!ipsSeen.Contains(ip))
+        //            ipsSeen.Add(ip);
+        //    }
+        //    return ipsSeen.Count;
+        //}
+
+    }
+}
