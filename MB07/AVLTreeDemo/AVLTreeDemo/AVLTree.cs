d2727aae177c8aa39f99eb111e92be09eb37d372
Thomas Kehl <tk@symas.ch>
2022-01-15 12:12:35 +0100

    MB07 added

---
+++ MB07/AVLTreeDemo/AVLTreeDemo/AVLTree.cs
@@ -0,0 +1,260 @@
+﻿using System;
+
+
+namespace AVLTreeDemo {
+    public enum TraverseModeEnum {
+        PreOrder,
+        PostOrder,
+        InOrder,
+        ReverseInOrder
+    }
+
+    public class AVLTree<T> where T : IComparable<T> {
+        internal AVLTreeNode<T> root;
+
+        public AVLTreeNode<T> Root => this.root;
+
+        public int Count { get; private set; }
+        public TraverseModeEnum TraverseMode { get; set; }
+
+        public AVLTree() {
+            TraverseMode = TraverseModeEnum.PreOrder;
+        }
+
+        public void Add(T item) {
+            if (root == null)
+                root = new AVLTreeNode<T>(item, null, this);
+            else
+                AddTo(root, item);
+
+            Count++;
+        }
+
+        public void AddRange(T[] items) {
+            foreach (var item in items)
+                Add(item);
+        }
+
+        private void AddTo(AVLTreeNode<T> node, T item) {
+            if (item.CompareTo(node.Item) < 0) {
+                if (node.Left == null) {
+                    node.Left = new AVLTreeNode<T>(item, node, this);
+                } else {
+                    AddTo(node.Left, item);
+                }
+            } else {
+                if (node.Right == null) {
+                    node.Right = new AVLTreeNode<T>(item, node, this);
+                } else {
+                    AddTo(node.Right, item);
+                }
+            }
+            // *** AddTo will recusivly be called. So, the node.Balance() will also be recusivly called to the root.
+            node.Balance();
+        }
+
+        public bool Contains(T item) {
+            // defer to the node search function.
+            return Search(item) != null;
+        }
+
+        public AVLTreeNode<T> Search(T item) {
+            var node = root;
+
+            while (node != null) {
+                var c = item.CompareTo(node.Item);
+
+                if (c == 0) {
+                    return node;
+                }
+
+                if (c < 0) {
+                    node = node.Left;
+                } else {
+                    node = node.Right;
+                }
+            }
+            return null;
+        }
+
+        
+        public void Clear() {
+            root = null;
+            Count = 0;
+        }
+
+        public override string ToString() {
+            var s = "";
+            var level = 0;
+
+            Traverse(root, level, ref s, null);
+
+            return s;
+        }
+
+        private void Traverse(AVLTreeNode<T> node, int level, ref string s, Action<T> action) {
+            if (node == null) {
+                return;
+            }
+
+
+            var reverse = TraverseMode == TraverseModeEnum.ReverseInOrder;
+
+            if (TraverseMode == TraverseModeEnum.PreOrder) {
+                s += "".PadLeft(level, ' ') + node.Item + "\n";
+                action?.Invoke(node.Item);
+            }
+            Traverse(reverse ? node.Right : node.Left, level + 2, ref s, action);
+
+            if (TraverseMode == TraverseModeEnum.InOrder || TraverseMode == TraverseModeEnum.ReverseInOrder) {
+                s += "".PadLeft(level, ' ') + node.Item + "\n";
+                action?.Invoke(node.Item);
+            }
+            Traverse(reverse ? node.Left : node.Right, level + 2, ref s, action);
+
+            if (TraverseMode == TraverseModeEnum.PostOrder) {
+                s += "".PadLeft(level, ' ') + node.Item + "\n";
+                action?.Invoke(node.Item);
+            }
+        }
+
+        public void Traverse(TraverseModeEnum mode, Action<T> action) {
+            var currentMode = TraverseMode;
+            TraverseMode = mode;
+            var s = "";
+            var level = 0;
+            Traverse(root, level, ref s, action);
+            TraverseMode = currentMode;
+        }
+
+        #region Remove
+        /// <summary>
+        /// Removes the first occurance of the specified value from the tree.
+        /// </summary>
+        /// <param name="value">The value to remove</param>
+        /// <returns>True if the value was removed, false otherwise</returns>
+        public bool Remove(T value) {
+            var current = this.Search(value);
+
+            if (current == null) {
+                return false;
+            }
+
+            var treeToBalance = current.Parent;
+
+            Count--;
+
+            // not leaf?
+            if (current.Right != null || current.Left != null) {
+                // Case 1: If current has no right child, then current's left replaces current
+                if (current.Right == null) {
+                    if (current.Parent == null) {
+                        root = current.Left;
+                        if (root != null) {
+                            root.Parent = null;
+                        }
+                    } else {
+                        // Determine, if the current node (which will be removed) is left or right of its parent
+                        int result = current.Item.CompareTo(current.Parent.Item);
+                        if (result < 0) {
+                            // if parent value is less than current value
+                            // make the current left child a left child of parent
+                            current.Parent.Left = current.Left;
+                            treeToBalance = current.Left;
+                        } else if (result > 0) {
+                            // if parent value is greater than current value
+                            // make the current left child a right child of parent
+                            current.Parent.Right = current.Left;
+                            treeToBalance = current.Left;
+                        }
+                    }
+                }
+                // Case 2: If current has no left child, then current's right replaces current
+                else if (current.Left == null) {
+                    if (current.Parent == null) {
+                        root = current.Right;
+                        if (root != null) {
+                            root.Parent = null;
+                        }
+                    } else {
+                        // Determine, if the current node (which will be removed) is left or right of its parent
+                        var result = current.Item.CompareTo(current.Parent.Item);
+                        if (result < 0) {
+                            // if parent value is less than current value
+                            // make the current right child a left child of parent
+                            current.Parent.Left = current.Right;
+                            treeToBalance = current.Right;
+                        } else if (result > 0) {
+                            // if parent value is greater than current value
+                            // make the current right child a right child of parent
+                            current.Parent.Right = current.Right;
+                            treeToBalance = current.Right;
+                        }
+                    }
+                }
+                // Case 3: If current's right child has a left child, replace current with current's
+                //         right child's left-most child
+                else {
+                    // find the right's left-most child
+                    AVLTreeNode<T> leftmost = current.Right.Left;
+
+                    if (leftmost == null) {
+                        leftmost = current.Right;
+                        // assign leftmost's left and right to current's left and right children
+                        leftmost.Left = current.Left;
+                    }
+                    else {
+                        while (leftmost.Left != null) {
+                            leftmost = leftmost.Left;
+                        }
+
+                        // the parent's left subtree becomes the leftmost's right subtree
+                        leftmost.Parent.Left = leftmost.Right;
+
+                        // assign leftmost's left and right to current's left and right children
+                        leftmost.Left = current.Left;
+
+                        leftmost.Right = current.Right;
+                    }
+
+                    if (current.Parent == null) {
+                        root = leftmost;
+                        if (root != null) {
+                            root.Parent = null;
+                        }
+                    } else {
+                        // Determine, if the current node (which will be removed) is left or right of its parent
+                        int result = current.Item.CompareTo(current.Parent.Item);
+                        if (result < 0) {
+                            // if parent value is less than current value
+                            // make leftmost the parent's left child
+                            current.Parent.Left = leftmost;
+                            treeToBalance = leftmost;
+                        } else if (result > 0) {
+                            // if parent value is greater than current value
+                            // make leftmost the parent's right child
+                            current.Parent.Right = leftmost;
+                            treeToBalance = leftmost;
+
+                        }
+                    }
+                }
+            }
+
+            if (treeToBalance != null) {
+                // Belancing is going to the top
+                while (treeToBalance != null) {
+                    var next = treeToBalance.Parent;
+                    treeToBalance.Balance();
+                    treeToBalance = next;
+                }
+            } else {
+                this.root?.Balance();
+            }
+
+            return true;
+        }
+        #endregion
+    }
+
+}
---
+++ MB07/AVLTreeDemo/AVLTreeView/MainWindow.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AVLTreeDemo;
+using AVLTreeView.BinaryTree;
+
+namespace AVLTreeView {
+    /// <summary>
+    /// Uses "Microsoft Automatic Graph Layout"
+    /// https://github.com/Microsoft/automatic-graph-layout 
+    /// https://en.wikipedia.org/wiki/Microsoft_Automatic_Graph_Layout 
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        AVLTree<int> _tree = new AVLTree<int>();
+        BinaryTree<int> _binaryTree = new BinaryTree<int>();
+        int currentCount = 0;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            RenderAvlTree(_tree);
+            RenderBinaryTree(_binaryTree);
+        }
+
+        private void RenderAvlTree(AVLTree<int> tree) {
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("");
+
+            AVLTreeNode<int> current = tree.Root;
+
+            RenderAvlTree(current, graph);
+
+            Microsoft.Msagl.GraphViewerGdi.GraphRenderer renderer = new Microsoft.Msagl.GraphViewerGdi.GraphRenderer(graph);
+            renderer.CalculateLayout();
+            System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap((int)avlImage.Width, (int)avlImage.Height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            renderer.Render(bitmap);
+
+            avlImage.Source = loadBitmap(bitmap);
+        }
+
+        private void RenderBinaryTree(BinaryTree<int> tree) {
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("");
+
+            BinaryTreeNode<int> current = tree.Head;
+
+            RenderBinaryTree(null, current, graph);
+
+            Microsoft.Msagl.GraphViewerGdi.GraphRenderer renderer = new Microsoft.Msagl.GraphViewerGdi.GraphRenderer(graph);
+            renderer.CalculateLayout();
+            System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap((int)binaryImage.Width, (int)binaryImage.Height, System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+            renderer.Render(bitmap);
+
+            binaryImage.Source = loadBitmap(bitmap);
+        }
+
+        private void RenderAvlTree(AVLTreeNode<int> node, Microsoft.Msagl.Drawing.Graph graph) {
+            if (node != null) {
+                RenderAvlTree(node.Right, graph);
+
+                if (node.Parent != null) {
+                    graph.AddEdge(node.Parent.Item.ToString(), node.Item.ToString());
+                }
+
+                RenderAvlTree(node.Left, graph);
+            }
+        }
+
+        private void RenderBinaryTree(BinaryTreeNode<int> parent, BinaryTreeNode<int> child, Microsoft.Msagl.Drawing.Graph graph) {
+            if (child != null) {
+                RenderBinaryTree(child, child.Right, graph);
+
+                if (parent != null) {
+                    graph.AddEdge(parent.Value.ToString(), child.Value.ToString());
+                }
+
+                RenderBinaryTree(child, child.Left, graph);
+            }
+        }
+
+
+        [DllImport("gdi32")]
+        static extern int DeleteObject(IntPtr o);
+
+        public static BitmapSource loadBitmap(System.Drawing.Bitmap source) {
+            IntPtr ip = source.GetHbitmap();
+            BitmapSource bs = null;
+            try {
+                bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(ip,
+                   IntPtr.Zero, Int32Rect.Empty,
+                   System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+            } finally {
+                DeleteObject(ip);
+            }
+
+            return bs;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e) {
+            string[] values = this.txtAddValue.Text.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string value in values) {
+                _binaryTree.Add(int.Parse(value.Trim()));
+                _tree.Add(int.Parse(value.Trim()));
+            }
+            RenderAvlTree(_tree);
+            RenderBinaryTree(_binaryTree);
+        }
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e) {
+            string[] values = this.txtRemoveValue.Text.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string value in values) {
+                _binaryTree.Remove(int.Parse(value.Trim()));
+                _tree.Remove(int.Parse(value.Trim()));
+            }
+
+            RenderAvlTree(_tree);
+            RenderBinaryTree(_binaryTree);
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e) {
+            _tree.Clear();
+            RenderAvlTree(_tree);
+
+            _binaryTree.Clear();
+            RenderBinaryTree(_binaryTree);
+        }
+
+        private void btnBad100_Click(object sender, RoutedEventArgs e) {
+            _tree.Clear();
+            _binaryTree.Clear();
+
+            for (int i = 0; i < 20; i++) {
+                _tree.Add(i);
+                _binaryTree.Add(i);
+            }
+
+            RenderAvlTree(_tree);
+            RenderBinaryTree(_binaryTree);
+        }
+
+        private void btnAdd100_Click(object sender, RoutedEventArgs e) {
+            Random rng = new Random();
+            for (int i = 0; i < 20; i++) {
+                int next = rng.Next();
+                while (_tree.Contains(next)) {
+                    next = rng.Next();
+                }
+
+                _tree.Add(next);
+                _binaryTree.Add(next);
+            }
+
+            RenderAvlTree(_tree);
+            RenderBinaryTree(_binaryTree);
+        }
+    }
+}
