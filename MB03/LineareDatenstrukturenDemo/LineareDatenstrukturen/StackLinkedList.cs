e9d7cc57e20ddcf61dffc514b9a6da1242495c7e
Thomas Kehl <tk@symas.ch>
2022-11-29 16:58:39 +0100

    added StackLinkedList

---
+++ MB03/LineareDatenstrukturenDemo/LineareDatenstrukturen/StackLinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineareDatenstrukturen {
+    public class StackLinkedList {
+        private DoublyLinkedList items;
+
+        public int Count {
+            get {
+                return items.Count;
+            }
+        }
+
+        public StackLinkedList() {
+            items = new DoublyLinkedList(); ;
+        }
+
+        public void Push(object item) {
+            items.Add(item);
+
+        }
+
+        public object Pop() {
+            if (Count == 0)
+                throw new InvalidOperationException("No items in stack.");
+
+            var ret = items[items.Count - 1];
+            this.items.Remove(ret);
+
+            return ret;
+        }
+
+        public object Peek() {
+            if (Count == 0)
+                throw new InvalidOperationException("No items in queue.");
+
+            return items[items.Count - 1];
+        }
+    }
+}
